f4cf81645d68fb0528f48dfa4685e42c70be96b2
Jago <jhon.gonzales2@unmsm.edu.pe>
2022-11-13 05:08:18 -0500

    correo logeado

--- src/app/layout/components/nav/feedback-modal/feedback-modal.component.ts
+++ src/app/layout/components/nav/feedback-modal/feedback-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ToastrService } from 'ngx-toastr';
+import { TokenService } from 'src/app/core/services/token.service';
 import { ClientService } from 'src/app/data/services/client.service';
 
 @Component({
@@ -9,11 +10,15 @@ import { ClientService } from 'src/app/data/services/client.service';
   styleUrls: ['./feedback-modal.component.scss'],
 })
 export class FeedbackModalComponent implements OnInit {
+  isLogged: boolean = false;
+  id: string | null = '';
+  
   constructor(
     public dialogRef: MatDialogRef<FeedbackModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
     private readonly clientService: ClientService,
-    private readonly toastr: ToastrService
+    private readonly toastr: ToastrService,
+    private tokenService: TokenService
   ) {}
 
   onNoClick(): void {
@@ -35,5 +40,53 @@ export class FeedbackModalComponent implements OnInit {
     console.log('no fin');
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getData();
+    this.fieldsValidate();
+  }
+
+  getData(): void {
+    this.id = this.tokenService.getIdFromToken();
+    if (!this.id) {
+      this.isLogged = false;
+      return;
+    }
+    this.clientService.getUserProfile(this.id).subscribe(
+      (res) => {
+        const { email, username } = res.body.data;
+        this.data.email = email;
+        this.data.name = username;
+        this.isLogged = true;
+      },
+      (err) => {
+        this.toastr.error(err.error.message, 'Error al cargar la información');
+        this.dialogRef.close();
+      }
+    );
+  }
+
+  fieldsValidate(): boolean {
+    if (this.data.name == '' || this.data.name == null) {
+      return false;
+    } else if (
+      this.data.email == '' ||
+      this.data.email == null ||
+      !this.isValidEmail()
+    ) {
+      return false;
+    } else if (this.data.message == '' || this.data.message == null) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  isValidEmail(): boolean {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(this.data.email);
+  }
+
+  isRequiredField(field: string): boolean {
+    return this.data[field] == '' || this.data[field] == null;
+  }
 }
